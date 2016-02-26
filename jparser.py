94fee68cfe03bd670a0ef8f8882c8aedbaf0bcef
Vitaly Akuloff <av@rdf.ru>
2016-02-26 12:22:23 +0300

    all ready to parse, next - command line arguments

--- jparser.py
+++ jparser.py
@@ -10,6 +10,49 @@ from __future__ import unicode_literals
 import sys
 import json
 
+
+# list of filter functions field__functionName=value
+FN_LIST = {
+    'eq': lambda x, y: str(x) == str(y),
+    'ne': lambda x, y: str(x) == str(y),
+}
+
+
+def parse_filter_string(s):
+    left, val = s.strip().split('=')
+    val = val.strip()
+
+    assert val
+
+    try:
+        left, fn_str = left.strip().split('__')
+        fn_str = fn_str.strip()
+    except ValueError:
+        fn_str = 'eq'
+
+    if not fn_str or fn_str not in FN_LIST.keys():
+        fn_str = 'eq'
+
+    fn = FN_LIST[fn_str]
+
+    keys = left.strip().split('.')
+    # keys = map(lambda x: x.strip(), keys)
+
+    return keys, fn, val
+
+
+def filter_line(keys, fn, val, **kwargs):
+    search_value = kwargs
+    for key in keys:
+        # здесь можно сделать .get() чтобы ингорировать несуществующие ключи
+        search_value = search_value[key]
+    return fn(search_value, val)
+
+
+def format_line(format_string, **kwargs):
+    return format_string.format(**kwargs)
+
+
 def parse_json(json_line):
     return json.loads(json_line)
 
--- tests.py
+++ tests.py
@@ -7,15 +7,39 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import unittest
-from jparser import parse_json
+
+from jparser import parse_json, format_line, filter_line, parse_filter_string
 
 
 class TestJParser(unittest.TestCase):
+    def setUp(self):
+        self.json_line = '{"@fields": {"uuid": "92c4c7cc-f00c-4c77-bc71-3687d33c225d", "level": "INFO", "status_code": 200, "content_type": "application/json", "path": "/v1/items/1/", "method": "PUT", "name": "django.http"}, "@timestamp": "2015-12-15T05:45:39+00:00", "@source_host": "c57872949172", "@message": "Request processed"}'
 
-    def test_parse(self):
-        json_line = '{"foo":"bar"}'
-        parsed_dict = parse_json(json_line)
-        self.assertEqual(parsed_dict['foo'], 'bar')
+    def test_parse_json(self):
+        parsed_dict = parse_json(self.json_line)
+        self.assertIsInstance(parsed_dict['@fields'], dict)
+        self.assertEqual(parsed_dict['@source_host'], 'c57872949172')
+
+    def test_format_line(self):
+        parsed_dict = parse_json(self.json_line)
+        sss = format_line("[{@timestamp}] {@fields[level]} {@message}", **parsed_dict)
+        self.assertEqual(sss, '[2015-12-15T05:45:39+00:00] INFO Request processed')
+
+    def test_parse_filter_string(self):
+        parsed_dict = parse_json(self.json_line)
+        keys, fn, val = parse_filter_string(' @fields.level = INFO ')
+
+        self.assertEqual(keys, ['@fields', 'level'])
+
+    def test_filter(self):
+        parsed_dict = parse_json(self.json_line)
+        keys, fn, val = parse_filter_string(' @fields.level = INFO ')
+        result = filter_line(keys, fn, val, **parsed_dict)
+        self.assertEqual(result, True)
+
+        keys, fn, val = parse_filter_string(' @fields.level = ZZZ ')
+        result = filter_line(keys, fn, val, **parsed_dict)
+        self.assertEqual(result, False)
 
 
 if __name__ == '__main__':
